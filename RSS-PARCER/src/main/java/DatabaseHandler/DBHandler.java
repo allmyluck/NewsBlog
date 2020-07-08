e6c9c056386211cb8207c188e02ccf2be9a0c4a2
mozerfucker <tokarevnikita08@gmail.com>
2020-07-08 12:10:15 +0300

    work: add fina parsing and InsertInDB

--- RSS-PARCER/src/main/java/DatabaseHandler/DBHandler.java
+++ RSS-PARCER/src/main/java/DatabaseHandler/DBHandler.java
@@ -5,10 +5,10 @@ import com.sun.syndication.feed.synd.SyndEntry;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DBHandler {
     private Connection connection;
-   // private Statement stmt;
     private final ArrayList<authors> currentAuthors;
 
     public DBHandler(ArrayList<authors> authors) {
@@ -20,38 +20,19 @@ public class DBHandler {
             String host = "jdbc:mysql://localhost:3306/";
             String url = host + "SimpleNewsFormat" + settings;
             this.connection = DriverManager.getConnection(url, user, password);
-          //  this.stmt = connection.createStatement();
         } catch(Exception error) {
             System.out.println("Connection failed: " + error);
         }
     }
 
 
-    // author -> entires
-    // проблемы с кодировкой
-    // нет проверки пока
-    public  void InsertArticle() throws SQLException {
-        for( Object classAuthor : currentAuthors) {
-            authors current = (authors)classAuthor;
-            ResultSet rs = GetLastElement(current.GetName());
-            for(Object objectEntry : current.getEntries()) {
-                SyndEntry currentEntry = (SyndEntry)objectEntry;
-                InsertInDatabase(currentEntry, current.GetName());
-                // проверка и добавление
-                //if(currentEntry.getPublishedDate().getTime() > rs.getLong("time") &&
-                //currentEntry.getLink() != rs.getString("link")) {
-                 //   InsertInDatabase(currentEntry, current.GetName());
-               // }
-            }
-        }
-    }
-
     private ResultSet GetLastElement(String name) throws SQLException {
         Statement stmt = connection.createStatement();
-        String query = String.format("SELECT * FROM news WHERE time = (SELECT MAX(time) FROM news WHERE author != NULL AND author = '%s');", name);
+        String query = String.format("SELECT * FROM news WHERE  time = (SELECT MAX(time) FROM news WHERE author = '%s');", name);
         //System.out.println(query);
         return stmt.executeQuery(query);
     }
+
 
     private void InsertInDatabase(SyndEntry current, String name) throws SQLException {
         Statement stmt = connection.createStatement();
@@ -62,5 +43,42 @@ public class DBHandler {
         //System.out.println(query);
         stmt.executeUpdate(query);
     }
+
+
+    public  void InsertArticle() throws SQLException {
+        for( Object classAuthor : currentAuthors) {
+            authors current = (authors)classAuthor;
+            ResultSet rs = GetLastElement(current.GetName());
+            List<String> links = new ArrayList<>();
+            List<Long> times = new ArrayList<>();
+            while (rs.next()) {
+                links.add(rs.getString("link"));
+                times.add(rs.getLong("time"));
+            }
+            for(Object objectEntry : current.getEntries()) {
+                SyndEntry currentEntry = (SyndEntry)objectEntry;
+                if(times.size() > 0 && currentEntry.getPublishedDate().getTime() > times.get(0)) {
+                    InsertInDatabase(currentEntry, current.GetName());
+                    //System.out.println("1");
+                } else if(times.size() > 0 && currentEntry.getPublishedDate().getTime() == times.get(0)) {
+                    boolean check = true;
+                    for(Object o : links) {
+                        String currentLink = (String)o;
+                        if(currentEntry.getLink().equals(currentLink)) {
+                            check = false;
+                        }
+                    }
+                    if(check) {
+                        InsertInDatabase(currentEntry, current.GetName());
+                        //System.out.println("2");
+                    }
+                } else if(times.size() == 0) {
+                    InsertInDatabase(currentEntry, current.GetName());
+                    //System.out.println("3");
+                }
+            }
+        }
+    }
+
 }
 
