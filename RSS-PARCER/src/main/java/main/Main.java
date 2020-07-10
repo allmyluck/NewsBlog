811346e1113b08f7f93e376cc133935af9d41a53
mozerfucker <tokarevnikita08@gmail.com>
2020-07-10 22:46:53 +0300

    work: change name authors

--- RSS-PARCER/src/main/java/main/Main.java
+++ RSS-PARCER/src/main/java/main/Main.java
@@ -9,8 +9,8 @@ public class Main {
     public static void main(String[] argc) {
         try {
             ArrayList<authors> myAuthors = new ArrayList<>();
-            myAuthors.add(new authors("meduza.io", new URL("https://meduza.io/rss/news")));
-            myAuthors.add(new authors("lenta.ru", new URL("https://lenta.ru/rss/news")));
+            myAuthors.add(new authors("meduza.io/rss/news", new URL("https://meduza.io/rss/news")));
+            myAuthors.add(new authors("lenta.ru/rss/news", new URL("https://lenta.ru/rss/news")));
             DBHandler handler = new DBHandler(myAuthors);
             handler.InsertArticle();
         } catch (Exception e) {
