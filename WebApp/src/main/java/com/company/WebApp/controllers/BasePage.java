48acf748073d89389e395a3c98df29406fa69a53
mozerfucker <tokarevnikita08@gmail.com>
2020-08-15 12:07:20 +0300

    Final fix

--- WebApp/src/main/java/com/company/WebApp/controllers/BasePage.java
+++ WebApp/src/main/java/com/company/WebApp/controllers/BasePage.java
@@ -4,20 +4,14 @@ import com.company.WebApp.models.Article;
 import com.company.WebApp.repositories.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
 
 @Controller
 public class BasePage {
@@ -25,8 +19,7 @@ public class BasePage {
     private ArticleRepository articleRepository;
 
     @GetMapping("/news")
-    public String NewsPage(HttpServletRequest request,
-                           Model model,
+    public String NewsPage(Model model,
                            @RequestParam(value = "page", required = false, defaultValue = "0") Integer page) {
         /*
         int amount = (int) articleRepository.count();
